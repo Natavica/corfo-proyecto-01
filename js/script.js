63c8dff89867ce50a6035c29f335d17648d11997
Natalia Albornoz <hola.natavica@gmail.com>
2024-10-21 00:04:03 -0300

    chore: ajustes de estilos en index y menu responsivo

--- js/script.js
+++ js/script.js
@@ -3,10 +3,9 @@ function toggleMenu() {
     navLinks.classList.toggle('show-menu');
 }
 
-// Cierra el menú cuando se selecciona una opción
 document.querySelectorAll('.navbar__links a').forEach(link => {
     link.addEventListener('click', () => {
         const navLinks = document.getElementById("navLinks");
-        navLinks.classList.remove("active"); // Cierra el menú
+        navLinks.classList.remove("active"); 
     });
 });
